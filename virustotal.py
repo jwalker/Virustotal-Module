40c86cb7c6e3ef40dcb7bc9a92f3e55ff241394e
Jacolon Walker <jaywalk.admin@gmail.com>
2014-04-03 08:35:06 +0000

    Removed unnecessary for loops to parse results. Added parse_resp() to parse response (resp) data

--- virustotal.py
+++ virustotal.py
@@ -17,78 +17,88 @@ class Virustotal():
 		self.base = "https://www.virustotal.com/vtapi/v2/"
 		self.apikey = "Your Virustotal APIKEY"
 
+
 	def rscReport(self, rsc):
 		""" Get latest report of resource """
 
-		buf = {}
 		base = self.base + 'file/report'
 		parameters = {"resource":rsc, "apikey":self.apikey}
 		r = requests.post(base, data=parameters)
 		resp = r.json()
-		for item in resp:
-			buf[item] = resp[item]
-
-		return buf
+		results = parse_resp(resp)
+		return results
 
 	def urlReport(self, rsc, scan=0):
 		""" Get latest report URL scan report of resource """
 
-		buf = {}
 		base = self.base + 'url/report'
 		parameters = {"resource":rsc, "scan":scan, "apikey":self.apikey}
 		r = requests.post(base, data=parameters)
 		resp = r.json()
-		for item in resp:
-			buf[item] = resp[item]
-
-		return buf
+		results = parse_resp(resp)
+		return results
 
 	# ipReport()/domainReport() both return 404 / Errors related to get to the API
 	def ipReport(self, rsc):
 		""" Get latest report for IP Address """
 
-		buf = {}
 		base = self.base + 'ip-address/report'
 		parameters = {"ip":rsc, "apikey":self.apikey}
 		r = requests.get(base, data=parameters)
 		resp = r.text
-		return resp
-		
+		results = parse_resp(resp)
+		return results
+
 	def domainReport(self, rsc):
 		""" Get latest report for IP Address """
 
-		buf = {}
 		base = self.base + 'domain/report'
 		parameters = {"domain":rsc, "apikey":self.apikey}
 		r = requests.post(base, data=parameters)
 		resp = r.text
-		return resp
+		results = parse_resp(resp)
+		return results
 		
 
 	def scanURL(self, rsc):
+
 		""" Send RSC/URL for scanning; Its encouraged to check for last scanusing urlReport()
 		To submit batch rsc should be example.com\nexample2.com"""
 
-		buf = {}
 		base = self.base + 'url/scan'
 		parameters = {"url":rsc, "apikey":self.apikey}
 		r = requests.post(base, data=parameters)
 		resp = r.json()
-		for item in resp:
-			buf[item] = resp[item]
-
-		return buf
+		results = parse_resp(resp)
+		return results
 
 	def rscSubmit(self, rsc):
+
 		""" Submit potential malicious file to virustotal for analyzing """
-		buf = {}
 		base = self.base + 'file/scan'
 		f = open(rsc, 'rb')
 		parameters = {"apikey":self.apikey}
 		r = requests.post(base, data=parameters, files={'file':f})
 		resp = r.json()
-		for item in resp:
-			buf[item] = resp[item]
+		results = parse_resp(resp)
+		return results
 
-		return buf
+	def rscRescan(self, rsc):
 
+		""" Rescan potential malicious file to virustotal for analyzing without uploading the file again """
+		base = self.base + 'file/rescan'
+		parameters = {"resource":rsc, "apikey":self.apikey}
+		r = requests.post(base, data=parameters)
+		resp = r.json()
+		results = parse_resp(resp)
+		return results
+
+
+def parse_resp(resp):
+	""" Parses the response from the requests.gets/posts()
+	then returns the data back to the function """
+	buf = {}
+	for item in resp:
+		buf[item] = resp[item]
+
+	return buf
